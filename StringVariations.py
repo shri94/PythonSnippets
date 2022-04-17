fdf3c49082ee610410cf7397c74020eccb707079
shri94 <ssuryanarayan94@gmail.com>
2022-04-17 14:25:23 -0400

    added 3 funcs

--- StringVariations.py
+++ StringVariations.py
@@ -1,8 +1,22 @@
 def exploringVariousSplits(text):
-    pass
+    print('List of words is text:', text.split(' '))
 
-def splitByDelimiter(text):
-    pass
+def splitByDelimiter(text, delimiter):
+    print(text.split(delimiter))
 
 def reverseString(text):
-    pass
+    print(text[::-1])
+
+if __name__ == '__main__':
+    text = "If sep is not specified or is None, a different splitting\
+    algorithm is applied: runs of consecutive whitespace are regarded\
+    as a single separator, and the result will contain no empty strings\
+    at the start or end if the string has leading or trailing whitespace.\
+    Consequently, splitting an empty string or a string consisting of\
+    just whitespace with a None separator returns []."
+
+    exploringVariousSplits(text)
+
+    splitByDelimiter(text, ',')
+    
+    reverseString(text)
